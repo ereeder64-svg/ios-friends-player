32109d46188681958cc75c7a95fc2dc702a14e3a
Eric Reeder <ereeder64@gmail.com>
2026-06-16 07:19:32 -0500

    Initial Commit

---
+++ Family PlayerTests/Family_PlayerTests.swift
@@ -0,0 +1,19 @@
+//
+//  Family_PlayerTests.swift
+//  Family PlayerTests
+//
+//  Created by Eric Reeder on 6/16/26.
+//
+
+import Testing
+@testable import Family_Player
+
+struct Family_PlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
